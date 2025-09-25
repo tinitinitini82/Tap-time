de8c3a674bd7def32d5d98ae5216eea764c1ad01
tinitinitini82 <valetroccolivis98@gmail.com>
2025-09-26 01:05:41 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'taptime-v1';
+const CACHE = 'taptime-v2';
 const ASSETS = ['.', './index.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
